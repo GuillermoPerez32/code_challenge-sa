36321fad99d77fa29b1a6fe521ef389e682b0e56
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-27 18:51:17 -0400

    feat: header

--- sa-ui/src/components/Header.tsx
+++ sa-ui/src/components/Header.tsx
@@ -1,9 +1,23 @@
+import { faAdd, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="table__header">
+      <h1>
+        Manage <span>Students</span>
+      </h1>
+      <div className="table__header--actions--container">
+        <div className="btn btn-danger t-bold">
+          <FontAwesomeIcon icon={faTrash} />
+          Delete
+        </div>
+        <div className="btn btn-accept t-bold">
+          <FontAwesomeIcon icon={faAdd} />
+          Add New Student
+        </div>
+      </div>
     </div>
   );
 };
