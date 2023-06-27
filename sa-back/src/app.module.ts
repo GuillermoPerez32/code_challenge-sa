ba7e74124155d2f99811c8db90cf511e038ab6e2
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-27 08:01:14 -0400

    config module

--- sa-back/src/app.module.ts
+++ sa-back/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StudentsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), StudentsModule],
 })
 export class AppModule {}
