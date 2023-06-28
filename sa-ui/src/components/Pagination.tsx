93d5a6e8d809beb9a63331d99a7a5e615e66748a
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2023-06-27 21:36:08 -0400

    feat: pagination

--- sa-ui/src/components/Pagination.tsx
+++ sa-ui/src/components/Pagination.tsx
@@ -1,5 +1,25 @@
 const Pagination = () => {
-  return <div>Pagination</div>;
+  const pages = 5;
+  const currentPage = 3;
+  return (
+    <div className="table__pagination--container">
+      <div className="table__pagination--count">
+        Showing 5 out of 25 entries
+      </div>
+      <div className="table__pagination--buttons">
+        <span>Previous</span>
+        {Array.from(Array(pages)).map((item, index) => (
+          <div
+            key={index}
+            className={`btn ${index === currentPage && "btn-pagination"}`}
+          >
+            {index}
+          </div>
+        ))}
+        <span>Next</span>
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
